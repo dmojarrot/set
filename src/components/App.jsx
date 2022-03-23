b74e077c9fcf42fdfbd836d648114fc43544fd3a
dmojarrot <a01638460@itesm.mx>
2022-03-23 17:37:59 -0600

    minor fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,11 @@ function App() {
         <Card />
         <Toaster />
       </div>
+
+      <div class='fixed right-0 bottom-0 pb-5 pr-2'>
+        <p className='text-purple-600 text-md'>In progress...</p>
+        <p className='text-zinc-300 text-xs'>One set at a time, then refresh</p>
+      </div>
     </div>
   )
 }
